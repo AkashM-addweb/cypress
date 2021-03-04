aea39dc56b3fe50350aceb53ce1bde54aa1b385b
Akash <akash.m.addweb@gmail.com>
2021-03-04 18:57:31 +0530

    new changes

--- cypress/integration/visual_testing/visuals.spec.js
+++ cypress/integration/visual_testing/visuals.spec.js
@@ -3,45 +3,43 @@
 
 describe('test suite for visual testing', function(){
 
-    // it('imageCompare', function(){
+    it('imageCompare', function(){
 
-    //     cy.visit('https://photographylife.com/what-is-photography')
-    //     cy.viewport('macbook-15')
-    //     cy.wait(3000)
-    //     cy.get('.fs-close-button').click()
-    //     // cy.wait('5000'   )
-    //     // cy.get('[src=https://photographylife.com/wp-content/uploads/2016/06/Brown-Anole-960x640.jpg]').matchImageSnapshot('promoHeader__promoMastheadLink')
-    //     cy.get('.wp-image-127898').matchImageSnapshot('promoHeader__promoMastheadLink')
-    // })
-
-
-
-    // it('image compare 2', function(){
-
-    //     cy.visit('https://css-tricks.com/')
-    //     cy.viewport('macbook-15')
-    //     cy.wait(3000)
-    //     cy.get('.fs-close-button').click()
-    //     // cy.wait('5000'   )
-    //     // cy.get('[src=https://photographylife.com/wp-content/uploads/2016/06/Brown-Anole-960x640.jpg]').matchImageSnapshot('promoHeader__promoMastheadLink')
-    //     cy.get('.wp-image-127898').matcclient/web?login=true#?hm=true&n=dfd81dc2-fbeb-3e78-8819-3c9d0d09d115&hImageSnapshot('.wp-image-127898')
-
-
-    // })
+        cy.visit('https://photographylife.com/what-is-photography')
+        cy.viewport('macbook-15')
+        cy.wait(3000)
+        cy.get('.fs-close-button').click()
+        // cy.wait('5000'   )
+        // cy.get('[src=https://photographylife.com/wp-content/uploads/2016/06/Brown-Anole-960x640.jpg]').matchImageSnapshot('promoHeader__promoMastheadLink')
+        cy.get('.wp-image-127898').matchImageSnapshot('promoHeader__promoMastheadLink')
+    })
 
 
 
-    // it('image compare 2', function(){
+    it('image compare 2', function(){
 
-    //     cy.visit('https://goaskalice.columbia.edu/')
-    //     cy.viewport('iphone-x')
-    //     cy.wait(3000)
-    //     cy.get('.navbar-toggler').should('be.visible')
-    //     // cy.wait('5000'   )
-    //     // cy.get('[src=https://photographylife.com/wp-content/uploads/2016/06/Brown-Anole-960x640.jpg]').matchImageSnapshot('promoHeader__promoMastheadLink')
-    //     // cy.get('.wp-image-127898').matchImageSnapshot('.wp-image-127898')
+        cy.visit('https://css-tricks.com/')
+        cy.viewport('macbook-15')
+        cy.wait(3000)
+        cy.get('.fs-close-button').click()
+        // cy.wait('5000'   )
+        // cy.get('[src=https://photographylife.com/wp-content/uploads/2016/06/Brown-Anole-960x640.jpg]').matchImageSnapshot('promoHeader__promoMastheadLink')
+        cy.get('.wp-image-127898').matcclient/web?login=true#?hm=true&n=dfd81dc2-fbeb-3e78-8819-3c9d0d09d115&hImageSnapshot('.wp-image-127898')
 
-    // })
+
+    })
+
+    it('image compare 2', function(){
+
+        cy.visit('https://goaskalice.columbia.edu/')
+        cy.viewport('iphone-x')
+        cy.wait(3000)
+        cy.get('.navbar-toggler').should('be.visible')
+        // cy.wait('5000'   )
+        // cy.get('[src=https://photographylife.com/wp-content/uploads/2016/06/Brown-Anole-960x640.jpg]').matchImageSnapshot('promoHeader__promoMastheadLink')
+        // cy.get('.wp-image-127898').matchImageSnapshot('.wp-image-127898')
+
+    })
 
 
     it('check_Visual_percy', function(){
